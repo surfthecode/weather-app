05b1db3f197bfadb0edde78aa7a651dec40be95c
surfthecode <surfthecode@gmail.com>
2023-06-16 13:53:45 +0300

    add icons; update script to get weather items from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,82 @@ const source = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=
 // fetch(source)
 //   .then((x) => x.text())
 //   .then((y) => (document.querySelector(".demo").innerHTML = y));
+let id = "";
+const iconURL = `https://openweathermap.org/img/wn/${id}@2x.png`;
 
 async function getData() {
   try {
-    const data = await fetch(source);
-    const result = await data.json();
+    const response = await fetch(source);
+    const data = await response.json();
 
-    if (data.ok) {
-      console.log("Success:", result);
+    if (response.ok) {
+      console.log("Success:", data);
+
+      // get city name
+      const cityName = data.city.name;
+      console.log(cityName);
+
+      // get current sunset time
+      const dateSet = new Date(data.city.sunset * 1000);
+      const sunset = dateSet.toLocaleString("en-GB", {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      console.log(sunset);
+
+      // get current sunrise time
+      const dateRise = new Date(data.city.sunrise * 1000);
+      const sunrise = dateRise.toLocaleString("en-GB", {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      console.log(sunrise);
+
+      // get current date and time
+      const currentDate = new Date().toLocaleString("en-GB", {
+        weekday: "short",
+        day: "numeric",
+        hour12: true,
+        hour: "numeric",
+        minute: "2-digit",
+      });
+
+      console.log(currentDate);
+
+      const currentDate2 = new Date().toISOString().slice(0, 10);
+      console.log(currentDate2);
+
+      // get current temp
+      const currentTemp = Math.floor(data.list[0].main.temp);
+      console.log(currentTemp);
+
+      // get current min temp
+      const currentTempMin = Math.floor(data.list[0].main.temp_min);
+      console.log(currentTempMin);
+
+      // get current max temp
+      const currentTempMax = Math.floor(data.list[0].main.temp_max);
+      console.log(currentTempMax);
+
+      // get current humidity
+      const currentHumidity = data.list[0].main.humidity + "%";
+      console.log(currentHumidity);
+
+      // get current weather description
+      const currentWeatherDescription = data.list[0].weather[0].description;
+      console.log(currentWeatherDescription);
+
+      // get current clouds
+      const currentClouds = data.list[0].clouds.all + "%";
+      console.log(currentClouds);
+
+      // get current weather icon code
+      const currentWeatherIconCode = data.list[0].weather[0].icon;
+      console.log(currentWeatherIconCode);
     } else {
-      console.log("Server error:", data.status, data.statusText);
+      console.log("Server error:", response.status, response.statusText);
     }
   } catch (error) {
     console.log("Fetch error:", error);
